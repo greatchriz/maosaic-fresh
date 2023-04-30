05da13b963a2ad8a0edf32cf581b1850657dba0c
greatchriz <89561544+greatchriz@users.noreply.github.com>
2023-04-30 12:21:47 +0100

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('account_number');
-            $table->float('account_balance')->default(100.00);
+            $table->float('account_balance')->default(50.00);
             $table->string('d_of_birth')->nullable();
             $table->string('gender')->nullable();
             $table->text('home_address')->nullable();
