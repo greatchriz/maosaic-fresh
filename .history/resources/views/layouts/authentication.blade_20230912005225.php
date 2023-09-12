2f547b6f0b0708ff4128238d7c9df4bfaaf164f3
greatchriz <89561544+greatchriz@users.noreply.github.com>
2023-09-13 00:13:12 +0100

    ertyuioliuyt

--- .history/resources/views/layouts/authentication.blade_20230912005225.php
+++ .history/resources/views/layouts/authentication.blade_20230912005225.php
@@ -89,8 +89,6 @@
 
         </main>
 
-
-
         <script
             src="/dash/js/vendors/alpinejs.min.js"
             defer
@@ -101,36 +99,5 @@
         <script src="/dash/js/fintech-charts.js"></script>
         <script src="/dash/js/vendors/flatpickr.js"></script>
         <script src="/dash/js/flatpickr-init.js"></script>
-
-
-        <div class="gtranslate_wrapper"></div>
-        <script>
-            window.gtranslateSettings = {"default_language":"en","detect_browser_language":true,"wrapper_selector":".gtranslate_wrapper","alt_flags":{"en":"usa"}}
-        </script>
-        <script
-            src="https://cdn.gtranslate.net/widgets/latest/float.js"
-            defer
-        ></script>
-
-        <script src="//code.tidio.co/plgg58xxs1n7vnhhrljafy4zrsvs8nhk.js" async></script>
-
-        <script
-            type="text/javascript"
-            src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-        ></script>
-
-        <!--Start of Tawk.to Script-->
-        <script type="text/javascript">
-            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-            (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/64ffc069b1aaa13b7a764f0c/default';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-            })();
-            </script>
-    <!--End of Tawk.to Script-->
     </body>
 </html>
